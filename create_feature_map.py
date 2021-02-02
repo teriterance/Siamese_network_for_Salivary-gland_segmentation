21d067f2fa05963f05ca14a1e29e0064ea2fd20c
gabin <gabinfodop@gmail.com>
2021-02-02 09:05:27 +0100

    insert img generation

--- create_feature_map.py
+++ create_feature_map.py
@@ -1,4 +1,5 @@
 import torch
+from contrastiveLoss import ContrastiveLoss
 
 
 def load_model(path='../model'):
@@ -6,12 +7,19 @@ def load_model(path='../model'):
     model.eval()
     return model
 
-def one_feature(image_ref, list_images, model):
+def one_feature(image_ref_pad, model, const_loss):
+    [size_x, size_y] = image_ref_pad.shape
+    img_out = image_ref_pad * 0
+    for i in range(15, size_x - 16):
+        for j in range(15, size_y - 16):
+            imagette = image_ref_pad[i-16:i+16, j-16:j+16]
+            img_out[i,j] = 
+
     list_val = []
     for img_small in list_images:
         out1, out2 = model(torch.from_numpy(img_small).float().unsqueeze(0), torch.from_numpy(image_ref).float().unsqueeze(0))
-        list_val.append(out)
 
+        list_val.append([out1, out2])
     return list_val
 
 def gen_feature_vector(img ,model, im_size = 32):
