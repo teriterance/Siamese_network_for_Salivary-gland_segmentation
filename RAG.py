c5048292be17f2764911977865e7ab6685092da0
gabin <gabinfodop@gmail.com>
2021-02-18 23:55:50 +0100

    update RAG

--- RAG.py
+++ RAG.py
@@ -8,12 +8,16 @@ class Heap:
         self.Heap = []
         self.Pair = []
         self.Where = []
+
+    def __str__(self) -> str:
+        return "Heap: "+str(self.Heap) + "\nPair: " + str(self.Pair) + "\nWhere: " +str(self.Where)
     
-    def construct(self, E, Where, Pair):
+    def construct(self, H, E, Where, Pair):
         """Construct class Heap"""
         s, e = int(E/2), E
         self.Where = Where.copy()
         self.Pair = Pair.copy()
+        self.Heap = H.copy()
         while s > 1:
             s = s - 1 
             self.shiftdown(s, e)
@@ -42,7 +46,7 @@ class Heap:
     def shiftup(self, s):
         """shiftup configuration"""
         i = s
-        j = 2*i
+        j = int(i/2)
 
         x, p = self.Heap[i], self.Pair[i]
         while j >= 1:
@@ -58,16 +62,17 @@ class Heap:
 
     def update(self, x, s, e):
         """Update function"""
-        if x > self.Heap(s):
-            self.Heap(s) = x
+        if x > self.Heap[s]:
+            self.Heap[s] = x
             self.shiftdown(s, e)
-        elif x < self.Heap(s):
-            self.Heap(s) = x
+        elif x < self.Heap[s]:
+            self.Heap[s] = x
             self.shiftup(s)
     
     def remove(self, x, s, e):
-        x = self.Heap(s)
-        self.Heap(s) = self.Heap(e)
+        """Remove function"""
+        x = self.Heap[s]
+        self.Heap[s] = self.Heap[e]
         
         p = self.Pair[e]
         self.Pair[s] = p
@@ -76,15 +81,70 @@ class Heap:
         if x < self.Heap[e]:
             self.shiftdown(s, e-1)
         elif x > self.Heap[e]:
-            self.shiftup()
+            self.shiftup(s)
 
-    def empty(self):
+    def _empty(self):
+        """clear the list"""
         self.Heap = []
         self.Pair = []
         self.Where = []
 
-def rag(img):
+class Adjency:
+    """"""
+    def __init__(self, im_shape,im_feature_1, im_feature_2):
+        ## chaque pixel va constituer une region
+        self.region_list = []
+        for i in range(im_shape[0]):
+            for j in range(im_shape[1]):
+                self.region_list.append([i,j])
+        ##construction du graphe
+        self.graph = {}
+        for a in range(len(self.region_list)):
+
+            if a[0] < 1:
+                self.graph[a] = [a[0]+1, a[1]]
+                if a[1] < 1: 
+                    self.graph[a] = [a[0], a[1]+1]
+                elif a[1] + 1 > im_shape[1]:
+                    self.graph[a] = [a[0], a[1]-1]
+
+            elif a[0] + 1 > im_shape[0]:
+                self.graph[a] = [a[0]-1, a[1]]
+                if a[1] < 1: 
+                    self.graph[a] = [a[0], a[1]+1]
+                elif a[1] + 1 > im_shape[1]:
+                    self.graph[a] = [a[0], a[1]-1]
+
+            elif a[0] > 1 and a[0] < im_shape[0] -1 and a[1] > 1 and a[1] < im_shape[1] - 1:
+                self.graph[a] = [a[0]-1, a[1]]
+                self.graph[a] = [a[0]+1, a[1]]
+                self.graph[a] = [a[0], a[1]-1]
+                self.graph[a] = [a[0], a[1]+1]
+        
+
+
+
+def RAG_val(img):
+    """RAG function"""
+
+    #Initialization
+    #each pixel is region
+
     pass
 
 if __name__ == "__main__":
-    pass
+    h = Heap()
+    print(h)
+    print("\n")
+    H = [1, 2, 3, 4]
+    P = [1, 2, 0, 3]#, [1,3]]
+    W = [1,0,2, 4]
+    h.construct(H, 4, W, P)
+    print(h)
+    print("\n")
+    h.update(2, 3, 3)
+    print(h)
+    print("\n")
+    h.remove(2, 3, 3)
+    print(h)
+    print("\n")
